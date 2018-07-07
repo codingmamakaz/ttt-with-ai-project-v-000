2ce3e6593091e4242889066ead1dde8c598b4620
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-07 04:59:22 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,26 @@
+require "pry"
+
 class Board
-end 
+
+  attr_accessor :cells
+
+  def reset!
+    cells.clear
+    self.cells = Array.new(9, " ")
+
+  end
+
+  def initialize
+    @cells = cells
+    self.cells = Array.new(9, " ")
+  end
+
+  def display
+    board = self.cells
+    puts board.to_a
+  end
+  #
+  # expected "X\nX\nX\nX\nO\nO\nX\nO\nO\n" to include " X | X | X "
+  # have to figure out how to get rid of \n and replace with |
+
+end
