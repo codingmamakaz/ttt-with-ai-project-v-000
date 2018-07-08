5e7acd48d0f586689afcead747c8913d4eabe838
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-08 03:02:25 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 class Board
 
   attr_accessor :cells
@@ -16,11 +14,52 @@ class Board
   end
 
   def display
-    board = self.cells
-    puts board.to_a
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
   end
-  #
-  # expected "X\nX\nX\nX\nO\nO\nX\nO\nO\n" to include " X | X | X "
-  # have to figure out how to get rid of \n and replace with |
+
+  def position(input)
+    input = input.to_i - 1
+    self.cells[input]
+  end
+
+  def full?
+    self.cells.all? do |cell|
+      if cell != " "
+        true
+      end
+    end
+  end
+
+  def turn_count
+    self.cells.count do |cell|
+       cell == "X" || cell == "O"
+    end
+  end
+
+  def taken?(input)
+    if position(input) == "X" || position(input) == "O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(input)
+    if input.to_i.between?(1, 9) && !taken?(input)
+      true
+    else
+      false
+    end
+  end
+
+  def update(num, player)
+    num = num.to_i - 1
+    player = "X" || "O"
+    self.cells[num] = player
+  end
 
 end
